3e2059e1c0b0cddca72769fdf0fae5d9c52f23d6
melchin.brad <brad@oldskoolstudios.com>
2022-08-19 10:26:29 -0600

    Premake | Game Engine Series | https://www.youtube.com/watch?v=sULV3aB2qeU&list=PLlrATfBNZ98dC-V-N3m0Go4deliWHPFwT&index=8&ab_channel=TheCherno

---
+++ premake5.lua
@@ -0,0 +1,114 @@
+workspace "Hazel" -- Solution Name
+    architecture "x64"
+    startproject "Sandbox" -- When we Right Click Sandbox Project and select "Set as Startup Project"
+    -- startup project is stored in .vs so this might have to manually set the startup project unless this folder gets deleted.
+
+    configurations
+    {
+        "Debug", -- No optimizations applied
+        "Release", -- Like Debug, with less Debug abilities and more optimizations (includes logging)
+        "Dist" -- Logging stript, this is what you release to the public
+    }
+
+-- List of macros (tokens) found here: https://github.com/premake/premake-core/wiki/Tokens
+-- cfg.buildcfg - Debug or Release
+-- cfg.system - Windows, Mac, etc.
+-- cfg.architecture - x64, x86, etc.
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Hazel"
+    location "Hazel" -- So we can reference from Hazel project and not from solution directory
+    kind "SharedLib" -- Type of file that this is (DLL)
+    language "C++"
+    
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files 
+    {
+        "%{prj.name}/src/**.h", -- Where to find the files for this project. Double ** searches children within that path.
+        "%{prj.name}/src/**.cpp"
+    }
+
+    includedirs
+    {
+        -- $(SolutionDir)Hazel\vendor\spdlog\include; From VS 
+        -- <Project> -> Properties -> C/C++ -> General -> Additional Include Directories
+        "%{prj.name}/vendor/spdlog/include"
+    }
+
+    filter "system:windows" -- Project Configuration for Windows platform specifically
+        cppdialect "C++17" -- <Project> -> Properties -> General -> C++ Language Standard
+        staticruntime "On" -- Linking the runtime libraries
+        systemversion "latest" -- Can find specific version at <Project> -> Properties -> General -> Windows SDK Version
+
+        defines -- <Project> -> Properties -> C/C++ -> Preprocessor -> Preprocessor Definitions
+        {
+            "HZ_PLATFORM_WINDOWS",
+            "HZ_BUILD_DLL"
+        }
+         
+        postbuildcommands --https://github.com/premake/premake-core/wiki/postbuildcommands
+        {
+            ("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox") -- Copies Hazel DLL's to Sandbox so that Sandbox can access it.
+        }
+    
+    filter "configurations:Debug"
+         defines "HZ_DEBUG"
+         symbols "On"
+    
+    filter "configurations:Release"
+         defines "HZ_RELEASE"
+         optimize "On"
+
+    filter "configurations:Dist"
+         defines "HZ_DIST"
+         optimize "On"
+
+
+project "Sandbox"
+    location "Sandbox"
+    kind "ConsoleApp" -- Type of file that this is (exe)
+    language "C++"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"Hazel/vendor/spdlog/include",
+		"Hazel/src"
+	}
+
+	links
+	{
+		"Hazel"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "10.0"
+
+		defines
+		{
+			"HZ_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines "HZ_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "HZ_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "HZ_DIST"
+		optimize "On" 
